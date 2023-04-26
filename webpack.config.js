98cb9d996886f9cc98b04416439bd3147ec9e6ce
Alina Mangusheva <alinamr@mail.ru>
2023-04-26 22:14:48 +0300

    feat: Настройка хэширования

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   devtool: 'source-map',
   plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'public'),
-          to: path.resolve(__dirname, 'build')
+          to: path.resolve(__dirname, 'build'),
+          globOptions: {
+            ignore: ['**/index.html'],
+          }
         }
       ]
     })

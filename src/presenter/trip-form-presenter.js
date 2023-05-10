c067ec7a7a6056c108632fc3b0e3f6b883af1390
Alina Mangusheva <alinamr@mail.ru>
2023-05-10 22:38:03 +0300

    feat: добавлены классам приватные поля и геттеры

--- src/presenter/trip-form-presenter.js
+++ src/presenter/trip-form-presenter.js
@@ -5,32 +5,36 @@ import {render} from '../framework/render.js';
 
 
 export default class TripFormPresenter {
-  routePointListComponent = new RoutePointListView();
+  #routePointsModel = null;
+  #destinationsModel = null;
+  #offersModel = null;
+  #routePointListContainer = null;
+  #routePointListComponent = new RoutePointListView();
+  #tripRoutePoints = [];
 
   constructor({routePointListContainer, routePointsModel, destinationsModel, offersModel}) {
-    this.routePointListContainer = routePointListContainer;
-    this.routePointsModel = routePointsModel;
-    this.destinationsModel = destinationsModel;
-    this.offersModel = offersModel;
+    this.#routePointListContainer = routePointListContainer;
+    this.#routePointsModel = routePointsModel;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
   }
 
   init() {
-    this.tripRoutePoints = [...this.routePointsModel.getRoutePoints()];
-    this.destinations = [...this.destinationsModel.getDestinations()];
+    this.#tripRoutePoints = [...this.#routePointsModel.routePoints];
 
-    render(this.routePointListComponent, this.routePointListContainer);
+    render(this.#routePointListComponent, this.#routePointListContainer);
 
 
     render(new EditFormView({
-      destination: this.destinationsModel.getById(this.tripRoutePoints[0]),
-      routePoint: this.tripRoutePoints[0],
-      offers: this.offersModel.getByType(this.tripRoutePoints[0]),
-    }), this.routePointListComponent.element);
+      destination: this.#destinationsModel.getById(this.#tripRoutePoints[0]),
+      routePoint: this.#tripRoutePoints[0],
+      offers: this.#offersModel.getByType(this.#tripRoutePoints[0]),
+    }), this.#routePointListComponent.element);
 
-    for (let i = 1; i < this.tripRoutePoints.length; i++) {
-      const destination = this.destinationsModel.getById(this.tripRoutePoints[i]);
-      const offers = this.offersModel.getById(this.tripRoutePoints[i]);
-      render(new RoutePointView({routePoint: this.tripRoutePoints[i], destination: destination, offers: offers}), this.routePointListComponent.element);
+    for (let i = 1; i < this.#tripRoutePoints.length; i++) {
+      const destination = this.#destinationsModel.getById(this.#tripRoutePoints[i]);
+      const offers = this.#offersModel.getById(this.#tripRoutePoints[i]);
+      render(new RoutePointView({routePoint: this.#tripRoutePoints[i], destination: destination, offers: offers}), this.#routePointListComponent.element);
     }
 
   }

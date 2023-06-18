bb7b02ffb6a3734c8e73707f033f17b20ca3b843
Alina Mangusheva <alinamr@mail.ru>
2023-06-19 00:31:22 +0300

    fix: внесение корректировок

--- src/const.js
+++ src/const.js
@@ -1,17 +1,3 @@
-const TYPES = ['taxi', 'flight', 'drive', 'check-in', 'sightseeing'];
-
-const CITY_NAMES = ['Amsterdam', 'Chamonix', 'Geneva', 'Rome', 'New York'];
-
-const DESCRIPTIONS = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-  'Aliquam erat volutpat',
-  'Nunc fermentum tortor ac porta dapibus',
-  'In rutrum ac purus sit amet tempus.',
-  'Cras aliquet varius magna, non porta ligula feugiat eget.'
-];
-
-const OFFERS = ['Order Uber', 'Add luggage', 'Switch to comfort', 'Rent a car', 'Add breakfast', 'Book tickets', 'Lunch in city'];
-
 const EMPTY_ROUTEPOINT = {
   id: '',
   basePrice: 0,
@@ -49,5 +35,5 @@ const UpdateType = {
   INIT: 'INIT',
 };
 
-export {TYPES, CITY_NAMES, DESCRIPTIONS, OFFERS, EMPTY_ROUTEPOINT, FilterType, SortType, UserAction, UpdateType};
+export { EMPTY_ROUTEPOINT, FilterType, SortType, UserAction, UpdateType };
 
